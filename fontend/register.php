7be7a181617f12874e561f65056f902c4af21a9d
Lauren DiDonato <lad4284@rit.edu>
2019-09-16 15:53:02 -0400

    reset and register page style fixes

--- fontend/register.php
+++ fontend/register.php
@@ -2,21 +2,24 @@
 
 
 <div class="form-register">
-  <h1 class="h3 mb-3 font-weight-normal">Register your account</h1>
+  <h1 class="h3 mb-3 font-weight-normal text-center top-20">Register</h1>
   <form action="register.php">
   
 	<label for="resetPW">Email address</label>
 	<input type="email" id="resetPW" class="form-control" required autofocus>
 	
-    <label for="myPW">Password</label>
+    <label for="myPW" class="top-20">Password</label>
     <input type="password" id="myPW" class="form-control" name="current" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
  
-    <label for="confirmPW">Confirm Password</label>
+    <label for="confirmPW" class="top-20">Confirm Password</label>
 	<input type="password" id="confirmPW" class="form-control" name="confirm" required>
 	<input type="checkbox" onclick="myFunction()"> &nbsp; Show Password
 	
-    <input class="btn btn-lg btn-primary btn-block" type="submit" value="Submit">
+    <input class="btn btn-lg btn-success btn-block top-20" type="submit" value="Submit">
   </form>
+     <div class="text-center top-20">
+          <p>Already have an account?<br><a href="login.php">Return to Login</a></p>
+      </div>
 </div>
 
 <div id="message">
