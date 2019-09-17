609f6a7cf79365acfc4c2acf5c8989022ed408ff
lym8703 <lym8703@g.rit.edu>
2019-09-17 11:21:36 -0400

    Fixed the footer for Admin page

--- fontend/admin.php
+++ fontend/admin.php
@@ -1,6 +1,7 @@
 <?php include 'header.php';?>
 
 <div class="container home-container">
+
     <div class="row">
     <!-- most outer row-->
         <div class="col-3">
@@ -11,9 +12,9 @@
             <!-- Left outer row -->
             <div class="card filter-card" style="width:100%">				
 				
-            <div class="card event-page-card">            
+				<div class="card event-page-card">            
 				
-                  <div class="input-group">
+					<div class="input-group">
 						
                       <div class="custom-file">
                         <input type="file" class="custom-file-input" id="inputGroupFile01"
@@ -23,44 +24,46 @@
                       </div>
                     </div>              
               
-            </div>
+				</div>
         			
 				</br>
-			<div class="card-body text-center d-flex justify-content-center align-items-center flex-column">
-				<p>Change Color [P]
-					<input type="color" value="#ff0000">
-				</p>  
-				
-				<p>Change Color [S] 
-					<input type="color" value="#ff0000">
-				</p>				
-				
-				<p>Change Color [T]
-					<input type="color" value="#ff0000">
-				</p>
-				</br>
-				<div>
-                        <button type="button" class="btn btn-link">Change Password? </button>
+				<div class="card-body text-center d-flex justify-content-center align-items-center flex-column">
+					<p>Change Color [P]
+						<input type="color" value="#ff0000">
+					</p>  
+					
+					<p>Change Color [S] 
+						<input type="color" value="#ff0000">
+					</p>				
+					
+					<p>Change Color [T]
+						<input type="color" value="#ff0000">
+					</p>
+					</br>
+					<div>
+                        <button type="button" class="btn btn-link">Change Password </button>
                     </div>
-			</div>		
+				</div>		
 			
 				<input class="btn btn-primary" type="submit" value="Submit">				
 					 
+			</div>
+			
 		</div>
-	</div>
 			        
-</div>
+	</div>
 	
 	<div class="col-9">
         <!--Right Content-->
             <div class="card program-list-card">
               <div class="card-header">                
-              </div>
+			  </div>
+			  
               <div class="card-body program-list-card-body">
                 <!--New User Approval-->
 					<div class="card event-card" id="event1">
                       <div class="card-body">
-                        <h5 class="card-title">New User</h5>
+                        <h5 class="card-title">New Users</h5>
                         <table class="table table-hover">
 							<thead>
 							<tr>
@@ -140,11 +143,13 @@
 						  </tbody>
 						</table>
 						
-               </div>
-            </div>
-        </div>
+						</div>
+					</div>
+				</div>
 		
-	</div>	
-
+			</div>	
+		</div>
+	</div>
+</div>
 		
 <?php include 'footer.php';?>
