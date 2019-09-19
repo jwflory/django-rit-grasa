9f13c2f62d7398af630deb059ec93e1d270add29
Lauren DiDonato <lad4284@rit.edu>
2019-09-19 15:59:39 -0400

    client side form validation for creating an event

--- fontend/createEvent.php
+++ fontend/createEvent.php
@@ -24,75 +24,100 @@
           </div>
         </div>
     </div>
-    <div class="row">
-        <div class="col-sm-8">
-            <div class="form-group">
-                <label>Program Name</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Name...">
-            </div>
-            <div class="form-group">
-                <label>Description</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Tell us about your program..."></textarea>
-            </div>
-            <div class="form-group">
-                <label>Location</label>
-                <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Street Address...">
-            </div>    
-
-            <div class="form-group">
-                <label>Website (optional)</label>
-                <input type="url" class="form-control" id="exampleFormControlInput1" placeholder="http://">
-            </div>  
-        </div>
-        <div class="col-sm-4">
-            <div class="form-group text-left multiBox">
-                <label>Activity</label><br>
-                <select class="custom-select w-100 activitySelect" id="basic" multiple="multiple">
-                </select>
-            </div> 
-            <div class="form-group">
-                <label>Transportation</label>
-                <select class="form-control">
-                    <option>Not Provided</option>
-                    <option>Provided</option>
-                </select>
-            </div> 
-            <div class="form-group text-left multiBox">
-                <label>Grades Served</label><br>
-                <select class="custom-select w-100 gradesSelect" id="basic" multiple="multiple">
-                </select>
-            </div> 
-            <div class="form-group">
-                <label>Gender</label>
-                <select class="form-control">
-                    <option>Not Specific</option>
-                    <option>Female</option>
-                    <option>Male</option>
-                </select>
-            </div>
-            <div class="form-group">
-                <label>Fees</label>
-                <div class="input-group mb-3">
-                  <div class="input-group-prepend">
-                    <span class="input-group-text">$</span>
-                  </div>
-                  <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)" placeholder="0.00">
+    
+    
+    
+    <form class="needs-validation" novalidate>
+      <div class="form-row">
+            <div class="col-sm-8">
+                <div class="form-group">
+                    <label>Program Name</label>
+                    <input type="text" class="form-control" placeholder="Name..." required>
+                    <div class="invalid-feedback">
+                        Please provide a name for your program.
+                    </div>
                 </div>
+                <div class="form-group">
+                    <label>Description</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" placeholder="Tell us about your program..." required></textarea>
+                    <div class="invalid-feedback">
+                        Please provide a description for your program.
+                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Location</label>
+                    <input type="text" class="form-control" id="exampleFormControlInput1" placeholder="Street Address..." required>
+                    <div class="invalid-feedback">
+                        Please provide your program's street address.
+                    </div>
+                </div>    
+                <div class="form-group">
+                    <label>Website (optional)</label>
+                    <input type="url" class="form-control" id="exampleFormControlInput1" placeholder="http://">
+                </div>  
             </div>
-            <div class="form-group text-left multiBox">
-                <label>Timing</label><br>
-                <select class="custom-select w-100 timingSelect" id="basic" multiple="multiple">
-                </select>
-            </div> 
-            
-                
+            <div class="col-sm-4">
+                <div class="form-group text-left multiBox">
+                    <label>Activity</label><br>
+                    <select class="form-control custom-select w-100 activitySelect" id="basic" multiple="multiple" required>
+                    </select>
+                    <div class="invalid-feedback">
+                        Please select at least one activity.
+                    </div>
+                </div> 
+                <div class="form-group">
+                    <label>Transportation</label>
+                    <select class="form-control" required>
+                        <option>Not Provided</option>
+                        <option>Provided</option>
+                    </select>
+                </div> 
+                <div class="form-group text-left multiBox">
+                    <label>Grades Served</label><br>
+                    <select class="custom-select w-100 gradesSelect" id="basic" multiple="multiple" required>
+                    </select>
+                    <div class="invalid-feedback">
+                        Please select at least one grade group.
+                    </div>
+                </div> 
+                <div class="form-group">
+                    <label>Gender</label>
+                    <select class="form-control" required>
+                        <option>Not Specific</option>
+                        <option>Female</option>
+                        <option>Male</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Fees</label>
+                    <div class="input-group mb-3">
+                      <div class="input-group-prepend">
+                        <span class="input-group-text">$</span>
+                      </div>
+                      <input type="number" class="form-control" aria-label="Amount (to the nearest dollar)" placeholder="0.00" required>
+                        <div class="invalid-feedback">
+                            Please enter total amount of fees. If the program is free enter 0.00
+                        </div>
+                    </div>
+                    
+                </div>
+                <div class="form-group text-left multiBox">
+                    <label>Timing</label><br>
+                    <select class="custom-select w-100 timingSelect" id="basic" multiple="multiple" required>
+                    </select>
+                     <div class="invalid-feedback">
+                        Please select at least one time.
+                    </div>
+                </div> 
+
+
+            </div>
         </div>
-    </div>
-    <div class="row float-right">
-        <div class="col-12">
-            <button type="button" class="btn btn-success"><i class="fa fa-check" aria-hidden="true"></i> Submit for Approval</button>
-        </div>
-    </div>
+        
+      <button class="btn btn-success float-right" type="submit"><i class="fa fa-check" aria-hidden="true"></i> Submit for Approval</button>
+    </form>
+    
+    
 </div>
 <script>
     
@@ -125,13 +150,69 @@
             if(words.length > 1){
                 //value names cannot have a slash in it or it won't show up
                var name = words[0].replace(/^\/|\/$/g, '')
-               console.log(name);
             }else{
                 var name = list[i]
             }
             $( "."+location).append( "<option value="+name+">"+list[i]+"</option>" );
         }
     }
+    
+    //Validation
+    (function() {
+      'use strict';
+      window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+          form.addEventListener('submit', function(event) {
+            if (form.checkValidity() === false) {
+              event.preventDefault();
+              event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+              
+            //validation style fix for multiselect
+            var multiList = document.getElementsByClassName("multiBox");
+            for(var i=0; i<multiList.length;i++){
+                var message = $(multiList[i]).find(".invalid-feedback")[0]
+                var button = $(multiList[i]).find(".btn-group").find(".multiselect")[0]
+                if($(message).css('display') == 'block'){
+                    $(button).css('border', 'solid 1px #dc3545')
+                }else{
+                    $(button).css('border', 'solid 1px #28a745')
+                    $(button).find("i").remove();
+                    $(button).append('<i class="fa fa-check" aria-hidden="true" style="color:#28a745;padding-top:5px;padding-right:5px;"></i>')
+                }
+                var liList = $(multiList[i]).find("li")
+                for(var x=0; x<liList.length;x++){
+                    var checkbox = $(liList[x]).find("input")[0]
+                    $(checkbox).change(function() {
+                        var parentBtn = $(this).parentsUntil(".multiBox")[4].firstChild
+                        var label = parentBtn.firstChild
+                        var txt = $(label).text()
+                        if(txt == "None selected"){
+                            //error make red and 
+                            $(parentBtn).css('border', 'solid 1px #dc3545')
+                            $(parentBtn).find("i").remove();
+                            $(parentBtn).append('<i class="fa fa-times" aria-hidden="true" style="color:#dc3545;padding-top:5px;padding-right:5px;"></i>')
+                        }else{
+                            //for more than one checkbox remove all then add one
+                            $(parentBtn).find("i").remove();
+                            $(parentBtn).css('border', 'solid 1px #28a745')
+                            $(parentBtn).append('<i class="fa fa-check" aria-hidden="true" style="color:#28a745;padding-top:5px;padding-right:5px;"></i>')
+                        }
+                    });
+                }
+            }
+            });
+          }, false); 
+        }, false);
+    })();
+    
+    //multi select validation issues continue
+
+    
 
 </script>
 <?php include 'footer.php';?>
