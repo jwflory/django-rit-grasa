55926223f8ec5677b84c729067b28a2c8950f37a
hxl5315 <hxl5315@rit.edu>
2019-09-23 17:01:24 -0400

    Altered development settings to match back end development

--- grasa_event_locator/settings/development.py
+++ grasa_event_locator/settings/development.py
@@ -34,8 +34,49 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 def show_toolbar(request):
     return not request.is_ajax() and request.user and request.user.is_superuser
 
-MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware", ]
-INSTALLED_APPS += ["debug_toolbar", ]
+# MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware", ]
+# INSTALLED_APPS += ["debug_toolbar", ]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'grasa_event_locator',
+]
+
+ROOT_URLCONF = 'grasa_program_locator.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'grasa_program_locator.wsgi.application'
+LOGIN_REDIRECT_URL = '/grasa_event_locator/templates/index.php'
 
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
@@ -61,7 +102,24 @@ DEBUG_TOOLBAR_PANELS = (
     'debug_toolbar.panels.redirects.RedirectsPanel',
 )
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 try:
     from local_settings import * # noqa
 except ImportError:
     pass
+
+STATIC_URL = '/static/'
