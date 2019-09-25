4b2bb8728a97a05b94d8ad8231901a1e2a847d9d
lym8703 <lym8703@g.rit.edu>
2019-09-25 11:23:34 -0400

    Edited header.php

--- fontend/header.php
+++ fontend/header.php
@@ -1,4 +1,5 @@
 <!doctype html>
+<html lang="en">
 <head>
     <!--Bootstrap Style-->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
