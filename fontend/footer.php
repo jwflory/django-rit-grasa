67ec668bfa7a23b29573e1008da100b2af905448
lym8703 <lym8703@g.rit.edu>
2019-09-27 12:25:31 -0400

    Added About and Contact pages
    Finished About and Contact pages, But still need to make contact form interactive.

--- fontend/footer.php
+++ fontend/footer.php
@@ -3,9 +3,9 @@
           <div class="container">
             <nav class="navbar navbar-expand-sm navbar-light">
                 <div class="navbar-nav footer-nav">
-                  <a class="nav-item nav-link" href="#">Monroe</a>
-                  <a class="nav-item nav-link" href="#">About</a>
-                  <a class="nav-item nav-link" href="#">Contact</a>
+                  <a class="nav-item nav-link" href="https://www.monroecounty.gov/" target="_blank">Monroe</a>
+                  <a class="nav-item nav-link" href="aboutContact.php">About</a>
+                  <a class="nav-item nav-link" href="aboutContact.php#contact">Contact</a>
                 </div>
             </nav>
           </div>
