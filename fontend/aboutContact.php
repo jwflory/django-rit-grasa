3d11b9d2e4ed0d22e884ae505f1a6add96f3d3fb
lym8703 <lym8703@g.rit.edu>
2019-10-01 11:01:55 -0400

    Edited some styling for Contact page

--- fontend/aboutContact.php
+++ fontend/aboutContact.php
@@ -61,7 +61,7 @@
 							<div class="form-row">
 								<div class="form-group col-md-12">
 									<label for="exampleInputEmail1">Email address</label>
-									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
+									<input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email *">
 									<small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
 								</div>
 							</div>
