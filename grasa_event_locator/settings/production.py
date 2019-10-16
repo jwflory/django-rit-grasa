1dc3ba0fd024041fcb6e70ffa5546915ab7d0cb7
Justin W. Flory <git@jwf.io>
2019-10-16 11:24:42 -0400

    mysql: Set sql_mode to STRICT_TRANS_TABLE
    This commit resolves a warning that crops up whenever we run the
    application:
    
    ```
    WARNINGS:
    ?: (mysql.W002) MySQL Strict Mode is not set for database connection
        'default'
    HINT: MySQL's Strict Mode fixes many data integrity problems in MySQL,
        such as data truncation upon insertion, by escalating warnings into
        errors. It is strongly recommended you activate it. See:
        https://docs.djangoproject.com/en/2.2/ref/databases/#mysql-sql-mode
    ```
    
    Here are some of my references for why I am making this change:
    
    * https://docs.djangoproject.com/en/2.2/ref/databases/#mysql-sql-mode
    * https://docs.djangoproject.com/en/2.2/ref/settings/#std:setting-DATABASES
    * https://docs.djangoproject.com/en/2.2/ref/settings/#std:setting-OPTIONS
    * https://stackoverflow.com/a/23023015/2497452
    
    I tested this change locally with `docker-compose` and verified the
    warning was resolved.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>

--- grasa_event_locator/settings/production.py
+++ grasa_event_locator/settings/production.py
@@ -13,6 +13,10 @@ DATABASES = {
         'PASSWORD': '',
         'HOST': '',
         'PORT': '',
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"
+        },
     },
 }
 
