8a50c7b864fb8acb53c6a39b5a64d679738462d3
Lauren DiDonato <lad4284@rit.edu>
2019-10-17 08:53:28 -0400

    Many UI Updates
    removed hover effect on tables
    made buttons unclickable for pending events in provider
    created a modal for creating new admin
    Hid lat long in create event along with more validation
    Style updates to the header
    added input and toast to change name in provider

--- grasa_event_locator/templates/admin.php
+++ grasa_event_locator/templates/admin.php
@@ -36,6 +36,43 @@
             </div>
         </div>
     </div>
+    
+    <!-- add admin modal-->
+        <div class="modal fade" id="addadminModal" tabindex="-1" role="dialog" aria-labelledby="addadminModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="addadminModalLabel">Add New Admin</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+            <form action="" method="post">
+              <div class="modal-body">
+                
+
+                    <label for="resetPW" class="top-20">Email address</label>
+                    <input type="email" id="resetPW" class="form-control" name="emailAddr" required autofocus>
+
+                    <label for="myPW" class="top-20">Password</label>
+                    <input type="password" id="myPW" class="form-control" name="current" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
+
+                    <label for="confirmPW" class="top-20">Confirm Password</label>
+                    <input type="password" id="confirmPW" class="form-control" name="confirm" required>
+                    <input id="SPcheckbox" type="checkbox" onclick="myFunction()">
+                    <label for="SPcheckbox">&nbsp; Show Password</label>
+
+                  
+                  
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                <button type="button" class="btn btn-success" type="submit" value="Submit">Create Admin</button>
+              </div>
+            </form>
+            </div>
+          </div>
+        </div>
 
 	<div class="twentyblock"></div>
 	<div class="row">
@@ -46,11 +83,11 @@
         
         <!--New User Approval-->
             <h4 class="top-20">Pending Users
-                <button type="button" class="btn btn-success float-right" id="addAdmin"> <i class="fa fa-plus" aria-hidden="true"></i> Add Admin</button>
+                <button type="button" class="btn btn-success float-right" id="addAdmin" data-toggle="modal" data-target="#addadminModal"> <i class="fa fa-plus" aria-hidden="true"></i> Add Admin</button>
                 <button type="button" class="btn btn-info float-right" id="allUsers"> <i class="fa fa-address-book" aria-hidden="true"></i> View All Users</button>
             
             </h4>
-                <table class="table table-hover table-bordered">
+                <table class="table table-bordered">
                   <thead class="thead-light">
                     <tr>
                       <th scope="col">Organization Name</th>
@@ -64,15 +101,15 @@
                     {% for pendingUser in pendingUserList %}
                       <th scope="row">{{ pendingUser.org_name }} - <a href="mailto:{{ pendingUser.user }}">{{ pendingUser.user }}</a></th>
                       <td>Pending</td>
-                      <td><a href="{% url 'approve_user' pendingUser.id %}"><button type="button" class="btn btn-outline-success">Approve</button></td></a>
-                      <td><a href="{% url 'deny_user' pendingUser.id %}"><button type="button" class="btn btn-outline-danger">Deny</button></td></a>
+                      <td><a href="{% url 'approve_user' pendingUser.id %}"><button type="button" class="btn btn-outline-success">Approve</button></a></td>
+                      <td><a href="{% url 'deny_user' pendingUser.id %}"><button type="button" class="btn btn-outline-danger">Deny</button></a></td>
                     </tr>
                     {% endfor %}
                   </tbody>
                 </table>
         <!--New Event Approval-->
             <h4 class="top-20">New Events</h4>
-                <table class="table table-hover table-bordered">
+                <table class="table  table-bordered">
                   <thead class="thead-light">
                     <tr>
                       <th scope="col">Event Name</th>
@@ -89,16 +126,16 @@
                       <th scope="row">{{ pendingEvent.title }}</th>
                       <td>{{ pendingEvent.user_id.org_name }}</td>
                       <td>Pending</td>
-                      <td><a href="{% url 'event_page' pendingEvent.id %}"><button type="button" class="btn btn-outline-info view-event">View</button></td></a>
-                      <td><a href="{% url 'approve_event' pendingEvent.id %}"><button type="button" class="btn btn-outline-success">Approve</button></td></a>
-                      <td><a href="{% url 'deny_event' pendingEvent.id %}"><button type="button" class="btn btn-outline-danger">Deny</button></td></a>
+                      <td><a href="{% url 'event_page' pendingEvent.id %}"><button type="button" class="btn btn-outline-info view-event">View</button></a></td>
+                      <td><a href="{% url 'approve_event' pendingEvent.id %}"><button type="button" class="btn btn-outline-success">Approve</button></a></td>
+                      <td><a href="{% url 'deny_event' pendingEvent.id %}"><button type="button" class="btn btn-outline-danger">Deny</button></a></td>
                     </tr>
                     {% endfor %}
                   </tbody>
                 </table>
         <!--New Event Approval-->
             <h4 class="top-20">Updated Events</h4>
-                <table class="table table-hover table-bordered">
+                <table class="table table-bordered">
                   <thead class="thead-light">
                     <tr>
                       <th scope="col">Event Name</th>
@@ -115,9 +152,9 @@
                       <th scope="row">{{ pendingEdit.title }}</th>
                       <td>{{ pendingEdit.user_id.org_name }}</td>
                       <td>Pending</td>
-                      <td><a href="{% url 'event_page' pendingEdit.id %}"><button type="button" class="btn btn-outline-info view-event">View</button></td></a>
-                      <td><a href="{% url 'approve_event' pendingEdit.id %}"><button type="button" class="btn btn-outline-success">Approve</button></td></a>
-                      <td><a href="{% url 'deny_event' pendingEdit.id %}"><button type="button" class="btn btn-outline-danger">Deny</button></td></a>
+                      <td><a href="{% url 'event_page' pendingEdit.id %}"><button type="button" class="btn btn-outline-info view-event">View</button></a></td>
+                      <td><a href="{% url 'approve_event' pendingEdit.id %}"><button type="button" class="btn btn-outline-success">Approve</button></a></td>
+                      <td><a href="{% url 'deny_event' pendingEdit.id %}"><button type="button" class="btn btn-outline-danger">Deny</button></a></td>
                     </tr>
                     {% endfor %}
                   </tbody>
