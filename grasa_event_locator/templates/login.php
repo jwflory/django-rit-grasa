86c95af7ded11e03036fa52b2db03c110c8c1698
lym8703 <lym8703@g.rit.edu>
2019-10-21 11:47:50 -0400

    Added the additional contact info
    Added the additional contact info on registration.php

--- grasa_event_locator/templates/login.php
+++ grasa_event_locator/templates/login.php
@@ -3,12 +3,26 @@
 	
     <form class="form-signin" method="post">{% csrf_token %}
       
-      <h1 class="h3 mb-3 font-weight-normal text-center">GRASA Programs Login</h1>
-      <p class="text-muted text-center">You do not need an account to browse events, only if you would like to add your own event.</p>
-      <label for="inputEmail" class="sr-only">Email address</label>
-      <input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" required autofocus>
-      <label for="inputPassword" class="sr-only">Password</label>
-      <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+    <h1 class="h3 mb-3 font-weight-normal text-center">GRASA Programs Login</h1>
+    <p class="text-muted text-center">You do not need an account to browse events, only if you would like to add your own event.</p>
+	  
+	  
+	<label for="inputEmail" class="sr-only">Email address</label>
+	<input type="email" id="inputEmail" class="form-control" placeholder="Email address" name="email" aria-describedby="errorEmail" required pattern="[^]+@[^]+[.][a-z]{2,63}$" autocomplete="email">
+			
+	<div class="alert-msg">	
+		Please use a valid email address.
+	</div>
+	  
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control" placeholder="Password" name="password" required>
+		
+		<div class="alert-msg">
+			The provided password doesn't match!
+			<button class="message__close" hidden data-closenotification>
+				<span class="message__closetext">Close message</span>
+			</button>
+		</div>
         
       
       <button class="btn btn-lg btn-success btn-block" type="submit">Login</button>
