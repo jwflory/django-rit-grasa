253d241fe024f3c3fb49f63c7ff2829e61cfb653
hxl5315 <hxl5315@rit.edu>
2019-10-28 19:54:16 -0400

    Forgot Password
    Models - Added in table for the reset urls.
    Base.py - Email information
    ChangePWLogout - A new page, mainly a copy of the change password page, with fields renamed for the new purpose. Modal removed, as it is not used in this context.
    resetPW - Hook up form to back end.
    URLs - add changePWLogout to list of URL's.
    views.py - Add code to send an email.

--- grasa_event_locator/templates/resetPW.php
+++ grasa_event_locator/templates/resetPW.php
@@ -3,10 +3,10 @@
 
   <div class="form-reset">
 	<h1 class="h3 mb-3 font-weight-normal text-center">Reset Password</h1>
-	<form action="resetPW.php">
-    
+	<form action="resetPW.php" method="post">
+	{% csrf_token %}
 		<label for="resetPW" class="sr-only">Email address</label>
-		<input type="email" id="resetPW" class="form-control" placeholder="Enter Email..." required autofocus>
+		<input type="email" id="resetPW" class="form-control" name="emailAddr" placeholder="Enter Email..." required autofocus>
         <small id="emailHelp" class="form-text text-muted">Please provide the email connected to your account.</small>
 		<input class="btn btn-lg btn-success btn-block top-20" type="submit" value="Submit">
         <p class="text-center top-20"><a href="login.php">Return to Login</a></p>
