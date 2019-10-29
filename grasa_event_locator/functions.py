5578eaf46bef74636bcccc3a1595de7f78828aab
hxl5315 <hxl5315@rit.edu>
2019-10-29 14:43:00 -0400

    Moved category writing function.
    Moved the commands that wrote the categories table to the functions.py file. Removed them from views.py

--- grasa_event_locator/functions.py
+++ grasa_event_locator/functions.py
@@ -1,4 +1,6 @@
 from django.core.mail import send_mail
+from .models import *
+from django.db import connection
 
 def send_email(address, subject, message):
     i = send_mail(
@@ -9,3 +11,86 @@ def send_email(address, subject, message):
         fail_silently=False,
     )
     return i
+
+def write_categories_table():
+    with connection.cursor() as cursor:
+        cursor.execute("delete from grasa_event_locator_program_categories")
+        cursor.execute("delete from grasa_event_locator_program")
+        cursor.execute("delete from grasa_event_locator_category")
+        cursor.execute("ALTER TABLE grasa_event_locator_category AUTO_INCREMENT = 1")
+    table = Category(description="Academic Support")
+    table.save()
+    table = Category(description="Arts and Culture")
+    table.save()
+    table = Category(description="Career or College Readiness")
+    table.save()
+    table = Category(description="Civic Engagement")
+    table.save()
+    table = Category(description="Community Service / Service Learning")
+    table.save()
+    table = Category(description="Entrepreneurship / Leadership")
+    table.save()
+    table = Category(description="Financial Literacy")
+    table.save()
+    table = Category(description="Health & Wellness")
+    table.save()
+    table = Category(description="Media Technology")
+    table.save()
+    table = Category(description="Mentoring")
+    table.save()
+    table = Category(description="Nature & the Environment")
+    table.save()
+    table = Category(description="Play")
+    table.save()
+    table = Category(description="Public Speaking")
+    table.save()
+    table = Category(description="Social and Emotional Learning (SEL)")
+    table.save()
+    table = Category(description="Sports and Recreation")
+    table.save()
+    table = Category(description="STEM")
+    table.save()
+    table = Category(description="Tutoring")
+    table.save()
+    table = Category(description="Other")
+    table.save()
+    # Refactored to match header and forms as well as fix duplicate search bug
+    table = Category(description="Not-Provided")
+    table.save()
+    table = Category(description="Provided")
+    table.save()
+    table = Category(description="K-3rd")
+    table.save()
+    table = Category(description="K-5th")
+    table.save()
+    table = Category(description="3rd-5th")
+    table.save()
+    table = Category(description="6th-8th")
+    table.save()
+    table = Category(description="9th-12th")
+    table.save()
+    # Refactored to match header and forms
+    table = Category(description="Non-Specific")
+    table.save()
+    # Refactored to match header and forms as well as fix duplicate search bug
+    table = Category(description="Female-Only")
+    table.save()
+    # Refactored to match header and forms as well as fix duplicate search bug
+    table = Category(description="Male-Only")
+    table.save()
+    # Refactored to match header and forms as well as fix duplicate search bug
+    table = Category(description="Before-School")
+    table.save()
+    # Refactored to match header and forms as well as fix duplicate search bug
+    table = Category(description="After-School")
+    table.save()
+    table = Category(description="Evenings")
+    table.save()
+    table = Category(description="Weekends")
+    table.save()
+    table = Category(description="Summer")
+    table.save()
+    # Refactored to "Other Time" to avoid conflicts with activities "Other" as well as fix duplicate search bug
+    table = Category(description="Other-Time")
+    table.save()
+    return table
