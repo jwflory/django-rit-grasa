09ec10404f224b2ac31c82de3bcb1a76f2baa7f0
hxl5315 <hxl5315@rit.edu>
2019-10-29 21:28:46 -0400

    Revert "Update header.php"
    This reverts commit 4835ce91e726a912ee8bd6a03dd6314665db67e1.

--- grasa_event_locator/templates/header.php
+++ grasa_event_locator/templates/header.php
@@ -66,7 +66,7 @@
 </script>
 <body class="d-flex flex-column h-100" cz-shortcut-listen="true">
     <nav class="navbar navbar-expand navbar-dark bg-dark">
-          <a class="navbar-brand" aria-label="GRASA-Logo" href="../search/">
+          <a class="navbar-brand" aria-label="GRASA-Logo" href="/index.php">
           <img id="header-logo" src="{% static "img/grasalogo.png" %}" alt="Logo">
         </a>
 
