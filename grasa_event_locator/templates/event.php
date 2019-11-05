1e02efc84f8e7c6e62602734f7e5e901fc8ca364
Lauren DiDonato <lad4284@rit.edu>
2019-11-05 10:53:02 -0500

    Lauren fe 8 (#149)
    * search page height issue fix
    
    * remove image from event page and style updates
    
    * remove image upload areas from provider and admin
    
    * typo fix

--- grasa_event_locator/templates/event.php
+++ grasa_event_locator/templates/event.php
@@ -11,41 +11,44 @@
             {% endfor %}
             </div>
             <p class="text-break">{{ event.content }}</p>
-            <p><b>Grades: </b> {{ grades_list_pub }} </p>
-            <p><b>Timing: </b> {{ timing_list_pub }}</p>
-            <p><b>Gender: </b> {{ gender_list_pub }}</p>
-            <p><b>Transportation: </b> {{ transportation_list_pub}}</p>
-            <p><b>Contact Information: </b> <br><i class="fa fa-user fa-fw" aria-hidden="true"></i> {{ event.contact_name }} <br><i class="fa fa-envelope fa-fw" aria-hidden="true"></i> {{ event.contact_email }} <br><i class="fa fa-phone fa-fw" aria-hidden="true"></i> {{ event.contact_phone }}</p>
+            
+            <h4>Location:</h4>
+            <p>
+            {% if event.address %}
+                <a target="_blank" href="https://maps.google.com/?q={{ event.address }}">{{ event.address }} <i class="fa fa-external-link" aria-hidden="true"></i></a>
+            {% else %}
+                <a>Location Not Provided</a>
+            {% endif %}
+            </p>
+            
+            <p><h4>Contact Information: </h4><p><i class="fa fa-user fa-fw" aria-hidden="true"></i> {{ event.contact_name }} <br><i class="fa fa-envelope fa-fw" aria-hidden="true"></i> {{ event.contact_email }} <br><i class="fa fa-phone fa-fw" aria-hidden="true"></i> {{ event.contact_phone }}</p>
 
 
         </div>
         <div class="col-sm-4">
-            <div class="card event-page-card">
-              <img src="https://via.placeholder.com/150" class="card-img-top" alt="Provider Logo">
-              <ul class="list-group list-group-flush">
-                {% if event.fees %}
-                <li class="list-group-item"><i class="fa fa-money fa-fw" aria-hidden="true"></i> ${{ event.fees }}</li>
-                {% else %}
-                <li class="list-group-item"><i class="fa fa-money fa-fw" aria-hidden="true"></i> Not Provided </li>
-                {% endif %}
-                {% if event.address %}
-                <a target="_blank" href="https://maps.google.com/?q={{ event.address }}"><li class="list-group-item"><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i>{{ event.address }}</li></a>
-                {% else %}
-                <a><li class="list-group-item"><i class="fa fa-map-marker fa-fw" aria-hidden="true"></i>Not Provided</li></a>
-                {% endif %}
-              </ul>
-              <div class="card-body">
-              {% if event.website %}
-                 <a target="_blank" href="{{ event.website }}" class="card-link"><i class="fa fa-globe fa-fw" aria-hidden="true"></i> {{ event.website }}</a>
+            <ul class="list-group">
+              <li class="list-group-item"><b>Transportation: </b> {{ transportation_list_pub}}</li>
+              <li class="list-group-item"><b>Grades: </b> {{ grades_list_pub }} </li>
+              <li class="list-group-item"><b>Gender: </b> {{ gender_list_pub }}</li>
+              {% if event.fees %}
+                <li class="list-group-item"><b>Fees: </b> ${{ event.fees }}</li>
               {% else %}
-                <div class="card-link"><i class="fa fa-globe fa-fw" aria-hidden="true"></i> Not Provided
+                <li class="list-group-item"><b>Fees: </b> Free</li>
               {% endif %}
-              </div>
-            </div>
+              <li class="list-group-item"><b>Timing: </b> {{ timing_list_pub }}</li>
+                  {% if event.website %}
+                  <li class="list-group-item"><a target="_blank" href="{{ event.website }}"><i class="fa fa-globe fa-fw" aria-hidden="true"></i> {{ event.website }}</a>
+                </li>
+                  {% else %}
+                    <li class="list-group-item ">
+                        <i class="fa fa-globe fa-fw" aria-hidden="true"></i> Website Not Provided
+                    </li>
+                  {% endif %}  
+            </ul>
         </div>
 
     </div>
 </div>
-</div>
+
 
 {% include "footer.php" %}
