0518a9ee527cf4f1a833b0536c0f9c16fa5a3193
leong96 <hxl5315@rit.edu>
2019-11-07 09:32:27 -0500

    Update provider.php
    Removed backtic.

--- grasa_event_locator/templates/provider.php
+++ grasa_event_locator/templates/provider.php
@@ -215,7 +215,7 @@
                   <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
                     <button type="submit" class="btn btn-primary" value="Submit">Save Changes</button>
-                  </div>`
+                  </div>
                </form>
         </div>
       </div>
