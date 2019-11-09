552607a593bbf3a3f463a0208b5f5a13d6b6d5d9
Justin W. Flory <4721034+jwflory@users.noreply.github.com>
2019-11-09 13:44:30 -0500

    Switch PHP extensions to HTML (closes #156) (#169)
    * Switch PHP extensions to HTML (closes #156)
    
    This commit is a large commit, but it refactors our files and code to
    change any existing PHP file extension to an HTML extension. This
    required a few changes, but here's the process I tried to follow:
    
    1. Deleted `index.php` - seems like a duplicate of `search/search.html`?
    2. If possible, wrap a page up into a `reverse()` method.
    3. Change hard-coded pointers to PHP pages to updated HTML path
    
    This could use testing and I left comments in some areas of the
    front-end code that I wasn't sure about.
    
    Closes #156.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>
    
    Update views.py
    
    Minor formatting changes that allow the app to start up again.
    
    Closed a parenthesis in def admin, removed a line break in the invite message.
    
    Signed-off-by: Harrison Leong
    
    * misc: Remove unused apps/base cruft
    
    This commit removes a large amount of code that is not being used in the
    project. This was originally related to how the template we started with
    was set up, but we ended up not using that structure.
    
    Related to #123.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>
    
    * settings: Consolidate duplicates, reformat with Black
    
    Related to #123.
    
    This commit mostly consolidates duplicates across our three config
    files. There were identical config settings repeated in `development.py`
    and `production.py` from `base.py`, so where there were duplicates in
    the child settings classes, I removed them.
    
    Additionally, this commit also reformats our settings files with the
    Black code linter, which is part of the work on cleaning up the code
    with #123 and #164.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>
    
    * Fix various misc. bugs with URLs and paths
    
    Sadly the `reverse('page_name')` prefixed a slash character to the
    string, so the following would evaluate as so:
    
        reverse('login_page') == /login.html
    
    Unfortunately, the slash throws off everything. I manually confirmed
    this by hard-coding the string with and without the slash. Unfortunately
    there is not enough time right now to dig into the root cause.
    
    Signed-off-by: Justin W. Flory <git@jwf.io>

--- grasa_event_locator/settings/development.py
+++ grasa_event_locator/settings/development.py
@@ -2,17 +2,12 @@ from .base import *  # noqa
 
 DEBUG = True
 
-INTERNAL_IPS = ["127.0.0.1"]
-
 SECRET_KEY = "secret"
 
 ALLOWED_HOSTS = ["*"]
 
-CACHES = {
-    "default": {
-        "BACKEND": "django.core.cache.backends.dummy.DummyCache"
-    }
-}
+CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
+
 
 # DJANGO DEBUG TOOLBAR SETTINGS
 # https://django-debug-toolbar.readthedocs.org
@@ -23,91 +18,49 @@ def show_toolbar(request):
 # MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware", ]
 # INSTALLED_APPS += ["debug_toolbar", ]
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
-
-
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'whoosh',
-    'haystack',
-    'grasa_event_locator',
-]
-
-ROOT_URLCONF = 'grasa_event_locator.urls'
-
-TEMPLATES = [
-    {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-            ],
-        },
-    },
-]
-
-WSGI_APPLICATION = 'grasa_event_locator.wsgi.application'
-LOGIN_REDIRECT_URL = '/grasa_event_locator/templates/index.php'
 
 DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
-    'HIDE_DJANGO_SQL': True,
-    'TAG': 'body',
-    'SHOW_TEMPLATE_CONTEXT': True,
-    'ENABLE_STACKTRACES': True,
-    'SHOW_TOOLBAR_CALLBACK': 'grasa_event_locator.settings.development.show_toolbar',
+    "INTERCEPT_REDIRECTS": False,
+    "HIDE_DJANGO_SQL": True,
+    "TAG": "body",
+    "SHOW_TEMPLATE_CONTEXT": True,
+    "ENABLE_STACKTRACES": True,
+    "SHOW_TOOLBAR_CALLBACK": "grasa_event_locator.settings.development.show_toolbar",
 }
 
 DEBUG_TOOLBAR_PANELS = (
-    'debug_toolbar.panels.versions.VersionsPanel',
-    'debug_toolbar.panels.timer.TimerPanel',
-    'debug_toolbar.panels.settings.SettingsPanel',
-    'debug_toolbar.panels.headers.HeadersPanel',
-    'debug_toolbar.panels.request.RequestPanel',
-    'debug_toolbar.panels.sql.SQLPanel',
-    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
-    'debug_toolbar.panels.templates.TemplatesPanel',
-    'debug_toolbar.panels.cache.CachePanel',
-    'debug_toolbar.panels.signals.SignalsPanel',
-    'debug_toolbar.panels.logging.LoggingPanel',
-    'debug_toolbar.panels.redirects.RedirectsPanel',
+    "debug_toolbar.panels.versions.VersionsPanel",
+    "debug_toolbar.panels.timer.TimerPanel",
+    "debug_toolbar.panels.settings.SettingsPanel",
+    "debug_toolbar.panels.headers.HeadersPanel",
+    "debug_toolbar.panels.request.RequestPanel",
+    "debug_toolbar.panels.sql.SQLPanel",
+    "debug_toolbar.panels.staticfiles.StaticFilesPanel",
+    "debug_toolbar.panels.templates.TemplatesPanel",
+    "debug_toolbar.panels.cache.CachePanel",
+    "debug_toolbar.panels.signals.SignalsPanel",
+    "debug_toolbar.panels.logging.LoggingPanel",
+    "debug_toolbar.panels.redirects.RedirectsPanel",
 )
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator"
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
-    },
+    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator"},
+    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator"},
+    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
 try:
-    from local_settings import * # noqa
+    from local_settings import *  # noqa
 except ImportError:
     pass
-
-STATIC_URL = '/static/'
