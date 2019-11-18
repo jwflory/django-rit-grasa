89098ec05312393dee36e6762f72e342f915101c
Justin W. Flory <git@jwf.io>
2019-11-18 16:52:42 -0500

    :wastebasket: Remove files/code not being used
    Part of #123.
    
    This commit does a lot of cruft cleanup, best summarized below:
    
    * gitignore: Remove all IntelliJ workspace settings, clean up rules
    * Purge `.idea/` directory, remove all files from GitHub
    * Remove old `up.sh` script since it is not used in current deployment
    * Remove/update settings file to minimize to bare essentials
    
    Signed-off-by: Justin W. Flory <git@jwf.io>

--- up.sh
+++
@@ -1,8 +0,0 @@
-git checkout remotes/origin/$1
-git pull
-sudo docker-compose stop -t 1 $2
-sudo docker-compose build $2
-sudo docker-compose create $2
-sudo docker-compose start $2
-
-sudo docker ps | grep $2
