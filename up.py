8aa10b8cb832741c7dbf60f21ed6a61aee7b964b
Nathaniel Larrimore <nlarrimore@datto.com>
2019-11-24 15:47:37 -0500

    cleaning up and polishing the startup script

--- up.py
+++ up.py
@@ -6,11 +6,12 @@ import subprocess
 import shlex
 
 parser = argparse.ArgumentParser()
-#group = parser.add_mutually_exclusive_group()
-parser.add_argument("--setup", help="initial setup", action='store_true')
-parser.add_argument("--start", help="start the app", action='store_true')
-parser.add_argument("--stop", help="stop the app", action='store_true')
-#parser.add_argument("-p", "--port", help="port to run the app on.")
+group = parser.add_mutually_exclusive_group()
+group.add_argument("--setup", help="initial setup", action='store_true')
+group.add_argument("--start", help="start the app", action='store_true')
+group.add_argument("--stop", help="stop the app", action='store_true')
+group.add_argument("--restart", help="Restart the container without rebuilding", action='store_true')
+parser.add_argument("-p", "--port", help="port to run the app on.", required=False)
 args = parser.parse_args()
 
 containerName = 'grasa_event_locator'
@@ -32,24 +33,59 @@ def cmd_run(command):
 
     return status, stdout, stderr
 
+def checkResults(results):
+    if results[0] != 0:
+        print('ran into an error.  Please check the docker logs for details')
+        print(results[2])
+        exit(1)
+
 def setup():
     print('building container:')
     results = cmd_run('docker build -t {} .'.format(imageName))
-    print(results[2])
-    results = cmd_run('sudo docker run -it --entrypoint "./manage.py" {} "migrate"'.format(imageName))
-    print(results[2])
+    checkResults(results)
+    
+    print('Migrating Database:')
+    results = cmd_run('sudo docker run --rm -it --entrypoint "./manage.py" {} "migrate"'.format(imageName))
+    checkResults(results)
+
+    print('Successfully build the container.')
 
 def start():
-    command = "docker run --name {name} -d -p {port}:8000 {uuid}".format(name=containerName, port='8000', uuid = imageName)
-    print(command)
+
+    if not args.port:
+        print('please specify the local port to run the container on using the -p/--port flag.')
+        exit(1)
+
+    print('starting...')
+    command = "docker run --rm --name {name} -d -p {port}:8000 {uuid}".format(name=containerName, port=args.port, uuid=imageName)
     results = cmd_run(command)
-    print(results[2])
-    results = cmd_run('yes | docker exec -i {} ./manage.py rebuild_index'.format(containerName))
-    print(results[2])
+    checkResults(results)
+
+    cmd_run('yes | docker exec -i {} ./manage.py rebuild_index'.format(containerName))
+
+    results = cmd_run('docker ps')
+    checkResults(results)
+    if containerName in results[1]:
+        print('Sucessfully started the container.')
+    else:
+        print('Could not start the container.  Please investigate.')
+
 
 def stop():
+    print('Stopping...')
     results = cmd_run("docker stop {}".format(containerName))
-    print(results[2])
+    checkResults(results)
+
+    results = cmd_run('docker ps')
+    checkResults(results)
+    if containerName not in results[1]:
+        print('Sucessfully stopped the container.')
+    else:
+        print('Could not stop the container.  Please investigate.')
+
+def restart():
+    stop()
+    start()
 
 if args.setup:
     setup()
@@ -57,3 +93,5 @@ elif args.start:
     start()
 elif args.stop:
     stop()
+elif args.restart:
+    restart()
